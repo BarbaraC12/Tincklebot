99e459814827217184ab8be13240e2efa06d50a9
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2022-09-24 18:52:05 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showString("https://learn.browndoggadgets.com/Guide/Ticklebot/303?lang=en")
+pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
+servos.P1.setAngle(90)
+servos.P2.setAngle(90)
+basic.forever(function () {
+    while (input.logoIsPressed()) {
+        servos.P1.setAngle(180)
+        servos.P2.setAngle(180)
+        basic.pause(200)
+        servos.P1.setAngle(90)
+        servos.P2.setAngle(90)
+        basic.pause(200)
+    }
+})
